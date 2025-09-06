26c94eb33cc7519f7cea2989a1a8b02869ddb3f4
Blastmerder <blastmerder@gmail.com>
2025-09-06 15:55:29 +0300

    solve bug loading workspaces

--- scripts/workspace.py
+++ scripts/workspace.py
@@ -2,6 +2,7 @@
 
 import subprocess
 from listener import listener
+from time import sleep
 
 images = {
     '$empty-select': "'images/workspaces/full/Select.svg'",
@@ -11,29 +12,33 @@ images = {
 }
 
 def get_workspaces():
-    command = 'bspc query -D -d .occupied --names'.split(' ')
-    out_put = subprocess.check_output(command).decode('utf-8').split('\n')
-    del out_put[-1]
+    try:
+        command = 'bspc query -D -d .occupied --names'.split(' ')
+        out_put = subprocess.check_output(command).decode('utf-8').split('\n')
+        del out_put[-1]
 
-    command = 'bspc query -D --names'.split(' ')
-    out_put1 = subprocess.check_output(command).decode('utf-8').split('\n')
-    del out_put1[-1]
+        command = 'bspc query -D --names'.split(' ')
+        out_put1 = subprocess.check_output(command).decode('utf-8').split('\n')
+        del out_put1[-1]
 
-    command = 'bspc query -D -d focused --names'.split(' ')
-    selected_desktop = subprocess.check_output(command).decode('utf-8')
-    icons = ['$empty-deselect'] * len(out_put1)
+        command = 'bspc query -D -d focused --names'.split(' ')
+        selected_desktop = subprocess.check_output(command).decode('utf-8')
+        icons = ['$empty-deselect'] * len(out_put1)
 
-    for desk in out_put:
-        icons[out_put1.index(desk)] = '$full-deselect'
+        for desk in out_put:
+            icons[out_put1.index(desk)] = '$full-deselect'
 
-    icons[out_put1.index(selected_desktop.replace('\n', ''))] = f'{icons[out_put1.index(selected_desktop.replace('\n', ''))].split('-')[0]}-select'
+        icons[out_put1.index(selected_desktop.replace('\n', ''))] = f'{icons[out_put1.index(selected_desktop.replace('\n', ''))].split('-')[0]}-select'
 
-    result = []
+        result = []
 
-    for i in range(len(out_put1)):
-        result.append(f'(button :onclick "bspc desktop -f {i+1}" :class "button" :style "background-image: url({images[icons[i]]}); padding-right: 5px;") ')
+        for i in range(len(out_put1)):
+            result.append(f'(button :onclick "bspc desktop -f {i+1}" :class "button" :style "background-image: url({images[icons[i]]}); padding-right: 5px;") ')
 
-    print(f'(box :class "workspaces_bar" :orientation "h" :space-evenly false :haligh "start" {"".join(result)})')
-    return f'(box :class "workspaces_bar" :orientation "h" :space-evenly false :haligh "start" {"".join(result)})'
+        print(f'(box :class "workspaces_bar" :orientation "h" :space-evenly false :haligh "start" {"".join(result)})')
+        return f'(box :class "workspaces_bar" :orientation "h" :space-evenly false :haligh "start" {"".join(result)})'
+    except:
+        sleep(1)
+        get_workspaces()
 
 listener(get_workspaces, "bspc subscribe desktop node_transfer".split(' '))
