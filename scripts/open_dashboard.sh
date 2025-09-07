463b7281898a79411e5b1d2dd9ab3b77cce06983
Blastmerder <blastmerder@gmail.com>
2025-09-07 19:55:17 +0300

    Minor updates for dashboard

--- scripts/open_dashboard.sh
+++ scripts/open_dashboard.sh
@@ -5,10 +5,10 @@ pop() {
     if [[ ! -f "$LOCK_FILE_SONG" ]]; then
         touch "$LOCK_FILE_SONG"
         eww open-many background \
-                        music_win
+                        music_win_dash
     else
         eww close background \
-                  music_win
+                  music_win_dash
         rm "$LOCK_FILE_SONG" && echo "closed"
     fi
 }
